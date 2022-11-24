e9b36d6510d2e9d108a1e7cd6ed33842e34aefaf
smirnovanton90 <smirnovanton90@gmail.com>
2022-11-24 17:34:08 +0300

    Edited main method

--- src/main/java/com/github/smirnovanton90/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/smirnovanton90/jrtb/JavarushTelegramBotApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
 	}
-
 }
